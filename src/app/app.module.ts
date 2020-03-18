5e6217211b534b1141f07621ae8d0fb0c479d50c
Rick Dawson <dawsora@gmail.com>
2020-03-18 10:49:41 -0700

    added background and initial navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    AboutComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
